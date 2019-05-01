e066e2144999f22736062ac24c3805d0f2c52b13
michaellikaiye <mye1@stuy.edu>
2019-05-01 11:00:35 -0400

    baseddddd

---
+++ gmath.py
@@ -0,0 +1,84 @@
+import math
+from display import *
+
+
+  # IMPORANT NOTE
+
+  # Ambient light is represeneted by a color value
+
+  # Point light sources are 2D arrays of doubles.
+  #      - The fist index (LOCATION) represents the vector to the light.
+  #      - The second index (COLOR) represents the color.
+
+  # Reflection constants (ka, kd, ks) are represened as arrays of
+  # doubles (red, green, blue)
+
+AMBIENT = 0
+DIFFUSE = 1
+SPECULAR = 2
+LOCATION = 0
+COLOR = 1
+SPECULAR_EXP = 4
+
+#lighting functions
+def get_lighting(normal, view, ambient, light, areflect, dreflect, sreflect ):
+    color = [0, 0, 0]
+    for j in range(3):
+        I = 0
+        I += ambient[j] * areflect[j]
+        I += light[1][j] * dreflect[j] * (dot_product(normalize(normal), normalize(light[0]))
+        I += light[1][j] * sreflect[j] * math.pow( (dot_product( () , normalize(view))) , SPECULAR_EXP)
+    return limit_color(color)
+
+def calculate_ambient(light, areflect):
+    pass
+
+def calculate_diffuse(light, dreflect, normal):
+    pass
+
+def calculate_specular(light, sreflect, view, normal):
+    pass
+
+def limit_color(color):
+    for i in range(3):
+        if color[i] < 0:
+            color[i] = 0
+        if color[i] > 255:
+            color[i] = 255
+    return color
+
+#vector functions
+#normalize vetor, should modify the parameter
+def normalize(vector):
+    magnitude = math.sqrt( vector[0] * vector[0] +
+                           vector[1] * vector[1] +
+                           vector[2] * vector[2])
+    for i in range(3):
+        vector[i] = vector[i] / magnitude
+def scale(vector, const):
+
+#Return the dot porduct of a . b
+def dot_product(a, b):
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+#Calculate the surface normal for the triangle whose first
+#point is located at index i in polygons
+def calculate_normal(polygons, i):
+
+    A = [0, 0, 0]
+    B = [0, 0, 0]
+    N = [0, 0, 0]
+
+    A[0] = polygons[i+1][0] - polygons[i][0]
+    A[1] = polygons[i+1][1] - polygons[i][1]
+    A[2] = polygons[i+1][2] - polygons[i][2]
+
+    B[0] = polygons[i+2][0] - polygons[i][0]
+    B[1] = polygons[i+2][1] - polygons[i][1]
+    B[2] = polygons[i+2][2] - polygons[i][2]
+
+    N[0] = A[1] * B[2] - A[2] * B[1]
+    N[1] = A[2] * B[0] - A[0] * B[2]
+    N[2] = A[0] * B[1] - A[1] * B[0]
+
+    return N
